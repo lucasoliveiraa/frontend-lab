2b7de307ad5133fae446f911ff16e97334d1d725
lucasoliveiraa <lucasjackcosta18@gmail.com>
2022-06-22 21:11:33 -0300

    chore: add tailwind

--- src/pages/app/index.tsx
+++ src/pages/app/index.tsx
@@ -1,5 +1,8 @@
 import { useUser, withPageAuthRequired } from "@auth0/nextjs-auth0";
-import { useGetProductsQuery } from "../../graphql/generated/graphql";
+import {
+  useGetProductsQuery,
+  useMeQuery,
+} from "../../graphql/generated/graphql";
 import {
   getServerPageGetProducts,
   ssrGetProducts,
@@ -9,19 +12,23 @@ import { withApollo } from "../../lib/withApollo";
 function Home({ data }) {
   const { user } = useUser();
   // const { data, loading, error } = useGetProductsQuery();
+  const { data: me } = useMeQuery();
 
   return (
     <div>
       <h1>Hello</h1>
-      <pre>{JSON.stringify(data.products, null, 2)}</pre>
       <pre>{JSON.stringify(user, null, 2)}</pre>
+      <pre>{JSON.stringify(me, null, 2)}</pre>
     </div>
   );
 }
 
 export const getServerSideProps = withPageAuthRequired({
   getServerSideProps: async (ctx) => {
-    return getServerPageGetProducts({}, ctx);
+    // return getServerPageGetProducts({}, ctx);
+    return {
+      props: {},
+    };
   },
 });
 
